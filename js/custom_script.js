5caef4df870c5e6927b59e4fff049a3f8e443e69
Chitra <chitratambi91@gmail.com>
2020-10-20 13:44:46 +0530

    All pages are done. Website complete.

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,25 +1,64 @@
-function validate_Signin_Form() {
-    var x = document.forms["mySigninForm"]["login"].value;
-    if (x == "") {
+//Forgot password form validation
+function validate_Forgot_Password_Form() 
+{
+  var x = document.forms["myForgotPasswordForm"]["new_password"].value;
+  if (x == "") 
+    {
+      alert("We share the link to reset the password. Check your email");
+      window.location.href = "login_pg.html";
+      return false;
+    }
+}
+
+//Login form validation
+function validate_Signin_Form() 
+{
+  var x = document.forms["mySigninForm"]["login"].value;
+  if (x == "") 
+    {
       alert("You have successfully logged in");
+      window.location.href = "index.html";
       return false;
     }
-  }
+}
 
-function validate_Signup_Form() {
-    var x = document.forms["mySignupForm"]["register"].value;
-    if (x == "") {
+//Registration form validation
+function validate_Signup_Form() 
+{
+  var x = document.forms["mySignupForm"]["register"].value;
+  if (x == "") 
+    {
       alert("You have successfully create an account");
+      window.location.href = "login_pg.html";
       return false;
     }
-  }
+}
 
-  $('.your-checkbox').prop('indeterminate', true)
-  
-  function validate_Publication_Form() {
-    var x = document.forms["myPublicationForm"]["publish"].value;
-    if (x == "") {
+//Checkbox validation in publication form
+$('.your-checkbox').prop('indeterminate', true)
+
+//Publication form validation
+function validate_Publication_Form() 
+{
+  var x = document.forms["myPublicationForm"]["publish"].value;
+  if (x == "") 
+    {
       alert("You have successfully post an article");
+      window.location.href = "index.html";
       return false;
     }
-  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
